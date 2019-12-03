efa0d3a0632677a8bfec78d487eddc57d90e92f9
lkatanic <lkatanic@ucsd.edu>
2019-12-02 21:55:37 -0800

    fixed play

--- play.py
+++ play.py
@@ -1,10 +1,10 @@
 # This is the file that calls Snake.py and runs the game
 
-from scripts import Snake
+from scripts import snake
 
 # This can be adjusted to choose the size of the game
 GAME_SIZE = 25
 
 # create new Snake object, then play game
-game = Snake(GAME_SIZE)
+game = snake.Snake(GAME_SIZE)
 game.play_game()
